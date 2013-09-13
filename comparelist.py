413567178c0e69c58cdf8a96a48028f9f5df23dc
V <vicki.boykis@gmail.com>
2013-09-12 21:55:14 -0400

    Compares two lists and returns the difference

---
+++ comparelist.py
@@ -0,0 +1,23 @@
+#compare two lists and print the differences
+
+
+#opens first file to compare
+with open("file1.txt") as file1list:
+        file1l = file1list.read().splitlines()
+       
+#opens second file to compare
+with open("file2.txt") as file2list:
+        file21 = file2list.read().splitlines()
+       
+#assigns files to sets
+set1=file1l
+set2=file2l
+
+#Prints everything in set b that's not in set a
+
+def difference(a, b):
+    return list(set(b).difference(set(a)))
+
+list_diff=difference(set1,set2)
+
+print list_diff
